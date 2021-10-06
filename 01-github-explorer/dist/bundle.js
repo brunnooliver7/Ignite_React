70c22b87cb35194f3fd1450b20da60c033abc987
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-10-06 18:04:06 -0300

    correcao 1.7

---
+++ 01-github-explorer/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\src\\index.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (4:8):\n\n\u001b[0m \u001b[90m 2 |\u001b[39m \u001b[36mimport\u001b[39m { render } \u001b[36mfrom\u001b[39m \u001b[32m'react-dom'\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 3 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 4 |\u001b[39m render(\u001b[33m<\u001b[39m\u001b[33mApp\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'root'\u001b[39m))\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m        \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\n    at Parser._raise (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:510:17)\n    at Parser.raiseWithData (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:503:17)\n    at Parser.expectOnePlugin (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:3383:18)\n    at Parser.parseExprAtom (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:11642:20)\n    at Parser.parseExprSubscripts (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:11217:23)\n    at Parser.parseUpdate (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:11197:21)\n    at Parser.parseMaybeUnary (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:11172:23)\n    at Parser.parseMaybeUnaryOrPrivate (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:10986:59)\n    at Parser.parseExprOps (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:10993:23)\n    at Parser.parseMaybeConditional (E:\\OneDrive\\Code\\Ignite_React\\01-github-explorer\\node_modules\\@babel\\parser\\lib\\index.js:10963:23)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,4 @@
 export function App() {
+    throw new Error('eita!')
     return <h1>Hello World JS</h1>
 }
