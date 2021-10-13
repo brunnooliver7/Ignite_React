7178f28fc8a60088ef6729f5c1a1a6258b377831
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-10-13 17:05:57 -0300

    2.1.6

--- 02-b-money/src/App.tsx
+++ 02-b-money/src/App.tsx
@@ -1,14 +1,9 @@
-import styled from 'styled-components';
 import { GlobalStyle } from './styles/global';
-
-const Title = styled.h1`
-  color: #8257e6;
-`
 
 export function App() {
   return (
     <div>
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
       <GlobalStyle />
     </div>
   );
